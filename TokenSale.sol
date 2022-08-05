a6a8b06c9f2cb4f37d127376e6734093ec48b2f3
MaybeNotXeph <93215707+xeph0@users.noreply.github.com>
2022-08-06 00:31:49 +0600

    Update TokenSale.sol

--- TokenSale.sol
+++ TokenSale.sol
@@ -8,74 +8,103 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 contract TokenSale is Ownable {
     uint256 public purchaseLimit;
     uint256 public rate;
-    uint256 public totalPurchase;
+
+    uint64 public constant CLIFF = 365 days;
+    address depositAddr;
+    bool public presale = true; 
+    bool public paused = false;
     
     IERC20 private constant USDC = IERC20(0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48); // Ethereum mainnet
     IERC20 public token; // Token to be sold
     
-    bool public presale = true; 
-    bool public paused = false;
-
-    mapping(address => uint) private _pBalance;
+    mapping(address => uint256) private _pBalance;
     mapping(address => bool) private _whitelist;
 
-    event TokenPurchase(address user, uint256 amountPurchased);
+    event TokensPurchased(address _holder, uint256 _amount);
+    event TokensVested();
     
     modifier notPaused() {
         require(paused == false, "PAUSED");
         _;
     }
 
-    constructor(address _token) {
+    constructor(address _token, address _depositAddr, uint256 _rate) {
         token = IERC20(_token);
+        depositAddr = _depositAddr;
+        rate = _rate;
     }
-
+    
+    /**
+     * @notice Purchase tokens
+     * @param amount the amount of tokens to be purchased including decimals
+     */
     function purchase(uint256 amount) external notPaused {
         require(amount <= contractBalance(), "INSUFFICIENT_TOKENS");
-        _pBalance[_msgSender()] += amount;
-        require(purchaseLimit > _pBalance[_msgSender()], "EXCEEDING_PURCHASE_LIMIT");
+        address msgSender = _msgSender();
+        _pBalance[msgSender] += amount;
+        require(purchaseLimit > _pBalance[msgSender], "EXCEEDING_PURCHASE_LIMIT");
 
         if(presale)
-            require(_whitelist[_msgSender()], "NOT_WHITELISTED");
-
-        totalPurchase += amount;
+            require(_whitelist[msgSender], "NOT_WHITELISTED");
         
         uint256 amountRp = ;
 
-        USDC.transferFrom(_msgSender(), owner(), amountRp);
+        USDC.transferFrom(msgSender, depositAddr, amountRp);
 
-        // Check against contract balance
+        emit TokensPurchased(msgSender, amount);
+
     }
 
     function contractBalance() public view returns (uint256) {
         return token.balanceOf(address(this));
     }
 
-    /* ||___ONLY-OWNER___|| */
+    /**
+     * @notice Check if an address is whitelisted
+     * @param user address to verify
+     */
+    function whitelisted(address user) external view returns (bool) {
+        return _whitelist[user];
+    }
 
+    /* ||___ ONLY-OWNER ___|| */
+    
+    /**
+     * @notice Include users into whitelist
+     * @param users list of addresses to whitelist
+     */
     function whitelist(
         address[] 
         calldata 
         users) 
     external onlyOwner {
-        for(uint16 i; i < users.length; i++)
+        for(uint256 i = 0; i < users.length; i++)
             _whitelist[users[i]] = true;
     }
 
-    function revokeWhitelist(
+    /**
+     * @notice Remove users from whitelist
+     * @param users list of addresses to remove from whitelist
+     */
+    function whitelistExclude(
         address[] 
         calldata 
         users) 
     external onlyOwner {
-        for(uint16 i; i < users.length; i++)
+        for(uint256 i = 0; i < users.length; i++)
             _whitelist[users[i]] = false;
     }
-
-    function setPurchaseLimit(uint256 _purchaseLimit) external onlyOwner {
-        purchaseLimit = _purchaseLimit;
+    
+    function setDepositAddress(uint256 newDepositAddr) external onlyOwner {
+        require(newDepositAddr != address(0), "NON_ZERO_REQ");
+        depositAddr = newDepositAddr;
     }
 
-    function enablePublicSale() external onlyOwner {
+    function setPurchaseLimit(uint256 newPurchaseLimit) external onlyOwner {
+        purchaseLimit = newPurchaseLimit;
+    }
+
+    function initPublicSale() external onlyOwner {
         presale = false;
     }
 
