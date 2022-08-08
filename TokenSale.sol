c12543ea5a290a3cff0769a51bbbd54a8c310921
MaybeNotXeph <93215707+xeph0@users.noreply.github.com>
2022-08-08 16:59:07 +0600

    Update TokenSale.sol

--- TokenSale.sol
+++ TokenSale.sol
@@ -6,13 +6,14 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract TokenSale is Ownable {
-    uint256 public purchaseLimit;
-    uint256 public rate;
+    uint public constant CLIFF_PERIOD = 365 days;
+    uint public constant VESTING_PERIOD = 365 days;
 
-    uint64 public constant CLIFF_PERIOD = 365 days;
-    uint64 public constant VESTING_PERIOD = 365 days;
-    uint64 public saleStartTime; // Remove if not relevant
-    uint64 public saleEndTime; // Remove if not relevant
+    uint public purchaseLimit;
+    uint public totalPurchased;
+    uint public rate;
+    uint public saleStartTime;
+    uint public saleEndTime;
 
     address private _vault;
     bool public presale = true; 
@@ -21,10 +22,18 @@ contract TokenSale is Ownable {
     IERC20 private constant USDC = IERC20(0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48); // Ethereum mainnet address
     IERC20 public token; // Token to be sold
     
-    mapping(address => uint256) private _pBalance;
-    mapping(address => bool) private _whitelist;
+    struct Allocation {
+        uint vestingStart;
+        uint amount;
+        uint released;
+    }
 
-    event TokensPurchased(address _holder, address _vestContract, uint256 _amount);
+    mapping(address => uint) private _pBalance;
+    mapping(address => bool) private _whitelist;
+    mapping(address => Allocation[]) private _allocations;
+
+    event TokensPurchased(address _holder, uint _amount);
+
     
     modifier notPaused() {
         require(paused == false, "PAUSED");
@@ -32,10 +41,10 @@ contract TokenSale is Ownable {
     }
 
     modifier onSale() {
-        require(saleStartTime > block.timestamp && block.timestamp < saleEndTime, "SALE_INACTIVE");
+        require(saleActive(), "SALE_INACTIVE");
         _;
     }
-
+    
     /**
      * @param _token address of token contract
      * @param vault_ address to send usdc received from sales
@@ -44,10 +53,9 @@ contract TokenSale is Ownable {
     constructor(
         address _token, 
         address vault_, 
-        uint256 _rate, 
-        uint256 vestingPeriod,
-        uint256 _saleStartTime,
-        uint256 _saleEndTime
+        uint _rate, 
+        uint _saleStartTime,
+        uint _saleEndTime
     ) {
         if(_token == address(0) || vault_ == address(0) || _rate == 0 ||
          _saleStartTime < block.timestamp || _saleStartTime > _saleEndTime)
@@ -55,39 +63,118 @@ contract TokenSale is Ownable {
 
         _vault = vault_;
         rate = _rate;
+        saleStartTime = _saleStartTime;
+        saleEndTime = _saleEndTime;
         token = IERC20(_token);
     }
     
     /**
      * @notice Purchase tokens and deploys a vesting contract
-     * @param amount the amount of tokens to be purchased
+     * @param _amount the amount of tokens to be purchased
      */
-    function purchase(uint256 amount) external onSale notPaused {
-        require(amount > 0 && contractBalance() > amount , "INVALID_AMOUNT");
+    function purchase(uint _amount) external onSale notPaused {
+        require(_amount > 0 && remainingTokens() > _amount , "INVALID_AMOUNT");
         address recipient = _msgSender();
         
         if(presale)
             require(_whitelist[recipient], "NOT_WHITELISTED");
 
-        _pBalance[recipient] += amount;
+        _pBalance[recipient] += _amount;
+
+        totalPurchased += _amount;
 
         require(purchaseLimit > _pBalance[recipient], "EXCEEDING_PURCHASE_LIMIT");
 
-        USDC.transferFrom(recipient, _vault, amount / rate);
+        USDC.transferFrom(recipient, _vault, _amount / rate);
         
-        address vestContract = 
+        _allocations[recipient].push(Allocation({
+            vestingStart: block.timestamp + CLIFF_PERIOD,
+            amount: _amount,
+            released: 0
+        }));
 
-        token.transfer(vestContract, amount);
+        emit TokensPurchased(recipient, _amount);
 
-        emit TokensPurchased(recipient, amount);
+    }
 
+    /**
+     * @dev Removes first allocation of caller
+     */
+    function _removeFront() internal {
+        Allocation[] storage allocations = _allocations[_msgSender()];
+        
+        if (allocations.length == 0) return;
+
+        for (uint i = 0; i < allocations.length-1; i++){
+            allocations[i] = allocations[i+1];
+        }
+
+        delete allocations[allocations.length-1];
+        allocations.pop();
+    }
+
+    /**
+     * @notice Claim tokens that have already vested.
+     */
+    function claim() external {
+        address claimant = _msgSender();
+        require(_pBalance[claimant] > 0, "NOT_HOLDER");
+        Allocation storage allocation = _allocations[claimant][0];
+        uint releasable = vestedAmount(block.timestamp) - allocation.released;
+        if(releasable == 0) revert("NO_TOKENS_TO_CLAIM");
+
+        allocation.released += releasable;
+        allocation.amount -= releasable;
+
+        if(allocation.amount == 0)
+            _removeFront();
+
+        token.transfer(claimant, releasable);
+    }
+
+    /**
+     * @dev Calculates the amount of tokens that has already vested. Default implementation is a linear vesting curve.
+     */
+    function vestedAmount(uint timestamp) public view virtual returns (uint) {
+        return _vestingSchedule(_allocations[_msgSender()][0].amount + _allocations[_msgSender()][0].released, timestamp);
+    }
+
+    /**
+     * @dev Virtual implementation of the vesting formula. This returns the amount vested, as a function of time, for
+     * an asset given its total historical allocation.
+     */
+    function _vestingSchedule(uint totalAllocation, uint timestamp) internal view virtual returns (uint) {
+        uint vStart = _allocations[_msgSender()][0].vestingStart;
+
+        if (timestamp < vStart) {
+            return 0;
+        } else if (timestamp > vStart + VESTING_PERIOD) {
+            return totalAllocation;
+        } else {
+            return (totalAllocation * (timestamp - vStart)) / VESTING_PERIOD;
+        }
     }
 
     /**
      * @notice Returns amount of tokens available for sale
      */
-    function contractBalance() public view returns (uint256) {
-        return token.balanceOf(address(this));
+    function remainingTokens() public view returns (uint) {
+        return token.balanceOf(address(this)) - totalPurchased;
+    }
+
+    /**
+     * @notice Returns usdc required to purchase input amount of tokens
+     * @param amount amount of tokens to purchase
+     */
+    function getCost(uint amount) public view returns (uint) {
+        return amount / rate;
+    }
+
+    /**
+     * @notice Returns if sale is currently ongoing
+     */
+    function saleActive() public view returns (bool) {
+        return saleStartTime > block.timestamp && block.timestamp < saleEndTime;
     }
 
     /**
@@ -109,7 +196,7 @@ contract TokenSale is Ownable {
         calldata 
         users) 
     external onlyOwner {
-        for(uint256 i = 0; i < users.length; i++)
+        for(uint i = 0; i < users.length; i++)
             _whitelist[users[i]] = true;
     }
 
@@ -122,7 +209,7 @@ contract TokenSale is Ownable {
         calldata 
         users) 
     external onlyOwner {
-        for(uint256 i = 0; i < users.length; i++)
+        for(uint i = 0; i < users.length; i++)
             _whitelist[users[i]] = false;
     }
     
@@ -139,7 +226,7 @@ contract TokenSale is Ownable {
      * @notice Change purchase limit
      * @param newPurchaseLimit new maximum amount of tokens a user can purchase
      */
-    function setPurchaseLimit(uint256 newPurchaseLimit) external onlyOwner {
+    function setPurchaseLimit(uint newPurchaseLimit) external onlyOwner {
         purchaseLimit = newPurchaseLimit;
     }
 
@@ -162,6 +249,33 @@ contract TokenSale is Ownable {
      */
     function unpause() external onlyOwner {
         paused = false;
+    }
+
+    /**
+     * @notice Change sale start time
+     * @param _startTime unix timestamp of new sale start time in seconds
+     * NOTE: Can only be called before sale has started
+     */
+    function setSaleStartTime(uint _startTime) external onlyOwner {
+        require(block.timestamp < saleStartTime, "SALE_STARTED");
+        saleStartTime = _startTime;
+    }
+
+    /**
+     * @notice Change sale end time
+     * @param _endTime unix timestamp of new sale end time in seconds
+     * NOTE: Can only be called before sale has ended
+     */
+    function setSaleEndTime(uint _endTime) external onlyOwner {
+        require(block.timestamp < saleEndTime, "SALE_ENDED");
+        saleEndTime = _endTime;
+    }
+
+    /**
+     * @notice Transfers unpledged tokens to contract owner
+     */
+    function withdrawTokens() external onlyOwner {
+        token.transfer(msg.sender, remainingTokens());
     }
 
 }
